a99bad1dc1651e3671cf5feead07c1fdf356c037
Alexander Botkin <alex.botkin@chargepoint.com>
2019-08-21 14:34:19 -0700

    Switch the Homebrew formula to build from source

--- Formula/xcparse.rb
+++ Formula/xcparse.rb
@@ -1,13 +1,18 @@
 class Xcparse < Formula
     desc "A command line tool to extract code coverage & screenshots from Xcode 11+ XCResult files."
     homepage "https://github.com/ChargePoint/xcparse"
-    url "https://github.com/ChargePoint/xcparse/raw/master/archive/xcparse.zip"
-    sha256 "765d0e3037e937079044c3751fad31dae1cc82659a7fb38bb61a8aaa23c0b90b"
+    url "https://github.com/ChargePoint/xcparse.git"
+    head "https://github.com/ChargePoint/xcparse.git"
     version "0.1.0"
-  
-    bottle :unneeded
-  
+    
+    depends_on :xcode => ["11.0", :build]
+    
     def install
-      bin.install "xcparse"
+        bin.mkpath
+        system "make", "install", "prefix=#{prefix}"
+    end
+    
+    test do
+        system "#{bin}/xcparse" "-h"
     end
   end
