6a6bc9b69e5e9393205cca93af384c3147443755
Alexander Botkin <alex.botkin@chargepoint.com>
2019-10-02 13:47:21 -0700

    Update the xcparse formula to use tag & fixed complaints from brew audit

--- Formula/xcparse.rb
+++ Formula/xcparse.rb
@@ -1,18 +1,17 @@
 class Xcparse < Formula
-    desc "A command line tool to extract code coverage & screenshots from Xcode 11+ XCResult files."
-    homepage "https://github.com/ChargePoint/xcparse"
-    url "https://github.com/ChargePoint/xcparse.git"
-    head "https://github.com/ChargePoint/xcparse.git"
-    version "0.1.0"
-    
-    depends_on :xcode => ["11.0", :build]
-    
-    def install
-        bin.mkpath
-        system "make", "install", "prefix=#{prefix}"
-    end
-    
-    test do
-        system "#{bin}/xcparse" "-h"
-    end
+  desc "Command-line extractor of screenshots/coverage from Xcode 11+ XCResult"
+  homepage "https://github.com/ChargePoint/xcparse"
+  url "https://github.com/ChargePoint/xcparse.git", :tag => "0.1"
+  head "https://github.com/ChargePoint/xcparse.git"
+
+  depends_on :xcode => ["11.0", :build]
+
+  def install
+    bin.mkpath
+    system "make", "install", "prefix=#{prefix}"
   end
+
+  test do
+    system "#{bin}/xcparse" "-h"
+  end
+end
